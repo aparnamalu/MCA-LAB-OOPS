38f4c990f4abae4a72614c877cedc9b56a99eb0b
Aparna JR <malua6414@gmail.com>
2026-03-28 12:06:35 +0530

    Add files via upload

---
+++ Authentication.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Authentication
+{
+    public static void main(String[] args) {
+        String username="Ashish";
+        String password="@Ashish123";
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the username");
+        String u1=sc.nextLine();
+        System.out.println("Enter the password");
+        String u2=sc.nextLine();
+        try
+        {
+            if((u1.equals(username))&&(u2.equals(password)))
+            {
+                System.out.println("Access Granted");
+
+            }
+            else
+            {
+                throw new CredentialException("Invalid Credentails");
+
+        }
+        }
+        catch(CredentialException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
