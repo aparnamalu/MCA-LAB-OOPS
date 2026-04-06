42c25d1c8c62c7b51855ed4eddf6f7d1b71be0bb
Aparna JR <malua6414@gmail.com>
2026-04-06 15:19:35 +0530

    Add files via upload

---
+++ DivException.java
@@ -0,0 +1,17 @@
+public class DivException
+{
+    static int a=10,b=0;
+    public static void main(String[] args) {
+        try{
+            System.out.println("Quotient:"+(a/b));
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Exception thrown:"+e);
+        }
+        finally{
+            b++;
+            System.out.println("Quotient"+(a/b));
+        }
+    }
+}
